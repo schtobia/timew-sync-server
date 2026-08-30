57f8efdc8bf84c4c4db4c9921ba65233ac2be86e
Lennart Kloock <lennart.kloock@protonmail.com>
2026-08-30 20:32:15 +0200

    fix: http method check

--- sync/handle.go
+++ sync/handle.go
@@ -32,7 +32,7 @@ var PublicKeyLocation string
 // HandleSyncRequest receives sync requests and starts the sync
 // process with the received data.
 func HandleSyncRequest(w http.ResponseWriter, req *http.Request, noAuth bool) {
-	if req.Method != http.MethodPost {
+	if req.Method != http.MethodPut {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
